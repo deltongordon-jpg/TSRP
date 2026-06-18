78397ee5dd4ab77d4b3a502b59a885d9a7f6d4a8
deltongordon-jpg <delton.gordon@gmail.com>
2026-06-17 20:44:41 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "https://discord.gg/EW2gmgDY3N",
+        name: "https://discord.gg/gmZCz8JqVs",
         // Activity type number (0 = Playing).
         type: 3,
       },
